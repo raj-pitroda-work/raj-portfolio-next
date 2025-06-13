bd470f7989c156a20f40c4700706955d50ec18b0
Pitroda Raj <pitrodarj@gmail.com>
2025-06-13 14:24:38 +0530

    og image changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import EditContextWrapper from "@/Components/Common/EditContextWrapper";
 import { ToastContainer } from "react-toastify";
+import ogImg from "../../public/opengraph-image.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,9 @@ export const metadata: Metadata = {
   title: "Raj Pitroda - Portfolio",
   description:
     "Expert in React JS and Next JS with 5 years of experience crafting dynamic, high-performance web applications, complemented by working knowledge of Node JS Nest JS for backend support.",
+  openGraph: {
+    images: [{ url: ogImg.src }],
+  },
 };
 
 export default function RootLayout({
